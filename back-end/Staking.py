a791ad23aa84fb63ff173035d8186110bf7c523f
gitcoin_yoon <socozy7210@gmail.com>
2022-06-12 02:34:32 +0900

    add bond-less/revoke metamask

--- back-end/Staking.py
+++ back-end/Staking.py
@@ -27,13 +27,24 @@ class MetaMask:
         
     def delegate(
         self, 
+        user_address,
         collator_address, 
-        delegator_address,
         delegate_amount, 
     ):
-        nonce = self.web3.eth.get_transaction_count(delegator_address)
+
+        '''
+        Method
+        - Send delegate tranasction
+
+        Params 
+        - collator_address: Whom to delegate
+        - user_address: Who is delegating
+        - delegate_amount: amount of delegate
+        '''
+
+        nonce = self.web3.eth.get_transaction_count(user_address)
         candidate_delgation_count = self.contract.functions.candidate_delegation_count(collator_address).call()
-        delegator_delegation_count = self.contract.functions.delegator_delegation_count(delegator_address).call()
+        delegator_delegation_count = self.contract.functions.delegator_delegation_count(user_address).call()
         tx_dict = self.contract.functions.delegate(
             collator_address,
             delegate_amount,
@@ -41,10 +52,88 @@ class MetaMask:
             delegator_delegation_count
         ).buildTransaction()
         tx_dict['nonce'] = nonce
-        tx_dict['from'] = delegator_address
-        signed_tx = self.web3.eth.account.sign_transaction(tx_dict, PRIVATE_KEY)
-        self.web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_dict['from'] = user_address
 
+        Helper.eth_sign_transaction(
+            web3=self.web3, 
+            tx_dict=tx_dict, 
+            user_address=user_address
+        )
+
+    def bond_more(self, user_address, collator_address, more):
+
+        '''
+        Method
+        - Send delegator-bond-more transaction.
+
+        Params 
+        - collator_address: Whom to delegate
+        - user_address: Who is delegating
+        - more: amount of more staking 
+        '''
+
+        nonce = self.web3.eth.get_transaction_count(user_address)
+        tx_dict = self.contract.functions.delegator_bond_more(
+            collator_address,
+            more,
+        )
+        tx_dict['nonce'] = nonce
+        tx_dict['from'] = user_address
+
+        Helper.eth_sign_transaction(
+            web3=self.web3,
+            tx_dict=tx_dict,
+            user_address=user_address
+        )
+
+    def bond_less(self, user_address, collator_address, less):
+        
+        '''
+        Method
+        - Send schedule-bond-less transaction
+
+        Params 
+        - user_address: user's public address
+        - collator_address: Whom to bond less
+        - less: amount of bond less
+        '''
+        nonce = self.web3.eth.get_transaction_count(user_address)
+        tx_dict = self.contract.functions.schedule_delegator_bond_less(
+            collator_address,
+            less,
+        )
+        tx_dict['nonce'] = nonce
+        tx_dict['from'] = user_address
+
+        Helper.eth_sign_transaction(
+            web3=self.web3,
+            tx_dict=tx_dict,
+            user_address=user_address
+        )
+
+    def revoke(self, user_address, collator_address):
+        
+        '''
+        Method
+        - Send scheduled-revoke-delegation transaction
+
+        Params
+        - user_address: User's public address
+        - collator_address: Whom to revoke
+        '''
+        nonce = self.web3.eth.get_transaction_count(user_address)
+        tx_dict = self.contract.functions.schedule_revoke_delegation(
+            collator_address,
+        )
+        tx_dict['nonce'] = nonce
+        tx_dict['from'] = user_address
+
+        Helper.eth_sign_transaction(
+            web3=self.web3,
+            tx_dict=tx_dict,
+            user_address=user_address
+        )
+        
 
 class Substrate:
 
@@ -55,21 +144,23 @@ class Substrate:
             print("Error connecting local node. Message: {error}".format(error=e))
             return 0
     
-    def bond(self, user_account, amount, payee):
+    def bond(self, user_address, amount, payee):
 
         '''
-        Send bond extrinsic using user's key pair
+        Method
+        - Send bond extrinsic 
         
         Params
-        - user_account(controller): address of user
+        - user_account(controller): user's public address 
         - amount: bond amount
         - payee: Staked(auto-compound) / Stash(Reward goes to stash account)
         '''
+
         generic_call = Helper.get_generic_call(
             module="Staking",
             function="bond",
             params={
-                'controller': user_account.ss58_address,
+                'controller': user_address.ss58_address,
                 'value': amount,
                 'payee': payee
             }
@@ -77,15 +168,17 @@ class Substrate:
         Helper.send_extrinsic(
             api=self.api,
             generic_call=generic_call,
-            user_account=user_account    
+            user_address=user_address    
         )
 
-    def nominate(self, user_account, validators):
+    def nominate(self, user_address, validators):
 
         '''
-        Send Nominate extrinsic using user's key pair
+        Method
+        - Send Nominate extrinsic 
 
         Params
+        - user_account: User's public address
         - validators: [address of Validators]
         '''
 
@@ -99,14 +192,15 @@ class Substrate:
         Helper.send_extrinsic(
             api=self.api,
             generic_call=generic_call,
-            user_account=user_account    
+            user_address=user_address    
         )
 
-    def bond_extra(self, user_account, additional): 
+    def bond_extra(self, user_address, additional): 
         
         '''
-        1. Send bond extra extrinsic using user's key pair
-        2. Call "putInFrontof" extrinsic to adjust user's bag position
+        Method
+        - Send bond extra extrinsic using user's key pair
+        - Call "putInFrontof" extrinsic to adjust user's bag position
         
         Params
         - additional: bond extra amount. 'Int'
@@ -121,13 +215,15 @@ class Substrate:
         Helper.send_extrinsic(
             api=self.api,
             generic_call=generic_call,
-            user_account=user_account    
+            user_address=user_address    
         )
         self._put_in_front_of(substrate_account="") # TO-DO 
 
-    def unbond(self, user_account, amount):
+    def unbond(self, user_address, amount):
+        
         '''
-        Send unbond extrinsic using user's key pair
+        Method
+        - Send unbond extrinsic using user's key pair
 
         Params
         - amount: unbond amount. 'Int'
@@ -142,12 +238,14 @@ class Substrate:
         Helper.send_extrinsic(
             api=self.api,
             generic_call=generic_call,
-            user_account=user_account    
+            user_address=user_address    
         )
 
-    def rebond(self, user_account, amount):
+    def rebond(self, user_address, amount):
+        
         '''
-        Send rebond extrinsic using user's key pair
+        Method
+        - Send rebond extrinsic using user's key pair
 
         Params
         - amount: unbond amount. 'Int'
@@ -162,19 +260,20 @@ class Substrate:
         Helper.send_extrinsic(
             api=self.api,
             generic_call=generic_call,
-            user_account=user_account    
+            user_address=user_address    
         )
 
-    def _put_in_front_of(self, user_account):
+    def _put_in_front_of(self, user_address):
         
         '''
-        Dispat call of "Bag-list" Pallet.
+        Method
+        - Dispath call of "Bag-list" Pallet.
 
         Params
         - lighter: address of whose point is lighter than user's
         '''
 
-        (user_score, list_head) = self._get_list_head(substrate_account=user_account)
+        (user_score, list_head) = self._get_list_head(substrate_account=user_address)
         curr_node = list_head;
         score = self._get_score(node=curr_node);
 
@@ -200,16 +299,23 @@ class Substrate:
         Helper.send_extrinsic(
             api=self.api,
             generic_call=generic_call,
-            user_account=user_account    
+            user_address=user_address    
         )
 
     def _get_list_head(self, substrate_account):
         
         '''
-        Get head of the bag-list
+        Method
+        - Internal method
+        - Get head of the bag-list
 
         Params
         - substrate_account: User's substrate account
+
+        Returns
+        - Tuple
+        - user_score: Score of user. Int 
+        - list_head: address of head. String
         '''
         
         list_node = self.api.query('VoterList', 'ListNodes', params=[substrate_account]).value
@@ -221,9 +327,35 @@ class Substrate:
         return (user_score, list_head)
 
     def _get_score(self, node):
+        
+        '''
+        Method
+        - Internal method
+
+        Params
+        - node: current node
+        
+        Returns
+        - Int
+        - score of node
+        '''
+
         return self.api.query('VoterList', 'ListNodes', params=[node]).value['score']
 
     def _get_next(self, node):
+        
+        '''
+        Method
+        - Internal method
+
+        Params
+        - node: current node
+
+        Returns
+        - String
+        - Next node
+        '''
+
         return self.api.query('VoterList', 'ListNodes', params=[node]).value['next']
 
 
@@ -235,7 +367,7 @@ if __name__ == "__main__":
     mnemonic = "seminar outside rack viable away limit tunnel marble category witness parrot eager"
     key_pair = Keypair.create_from_mnemonic(mnemonic=mnemonic)
     substrate.rebond(
-        user_account=key_pair,
+        user_address=key_pair,
         amount=1000000000000,
     )
     
